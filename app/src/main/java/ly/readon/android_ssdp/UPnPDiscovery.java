d49e0c0ba6d27626ec4f75ccf7ec9d91dcd9428a
Jonathan Fether <jfether@users.noreply.github.com>
2022-08-12 00:02:24 -0700

    Fixups for functional issues and deprecation
    Rewrote the implementation to fix some issues which prevented functionality
    Removed deprecated android.os.AsyncTask dependency. This also provides an asynchronous API.
    Split out the various Service Type (ST) searches.

--- app/src/main/java/ly/readon/android_ssdp/UPnPDiscovery.java
+++ app/src/main/java/ly/readon/android_ssdp/UPnPDiscovery.java
@@ -2,97 +2,109 @@ package ly.readon.android_ssdp;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
-import android.os.AsyncTask;
 
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Bernd Verst(@berndverst)
+ * @author Jonathan Fether (@jfether)
  */
-public class UPnPDiscovery extends AsyncTask
-{
-    HashSet<String> addresses = new HashSet<>();
-    Context ctx;
 
-    public UPnPDiscovery(Context context) {
-        ctx = context;
+public class UPnPDiscovery
+{
+    public static String ST_ALL = "ssdp:all";
+    public static String ST_ROOTDEVICE = "upnp:rootdevice";
+    public static String ST_AVTRANSPORT = "urn:schemas-upnp-org:service:AVTransport:1";
+
+    public static Future<String[]> discoverDevicesAsync(Context ctx, String serviceType) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+
+        return executor.submit(() -> {
+            HashSet<String> addresses = new HashSet<>();
+            WifiManager wifi = (WifiManager)ctx.getSystemService(Context.WIFI_SERVICE);
+
+            if(wifi != null) {
+
+                WifiManager.MulticastLock lock = wifi.createMulticastLock("UPnP Discovery Lock");
+                lock.acquire();
+
+                DatagramSocket socket;
+
+                try {
+                    InetAddress group = InetAddress.getByName("239.255.255.250");
+                    int port = 1900;
+                    String query =
+                            "M-SEARCH * HTTP/1.1\r\n" +
+                                    "Host: 239.255.255.250:1900\r\n"+
+                                    "ST: " + serviceType + "\r\n"+
+                                    "Man: \"ssdp:discover\"\r\n"+
+                                    "MX: 3\r\n"+
+                                    "\r\n";
+                    socket = new DatagramSocket();
+                    socket.setSoTimeout(300);
+
+                    DatagramPacket dgram = new DatagramPacket(query.getBytes(), query.length(),
+                            group, port);
+                    socket.send(dgram);
+
+                    long time = System.currentTimeMillis();
+                    long curTime = System.currentTimeMillis();
+
+                    // Let's consider all the responses we can get in 1 second
+                    while (curTime - time < 1000) {
+
+                        byte[] receiveData = new byte[256];
+                        DatagramPacket p = new DatagramPacket(receiveData, receiveData.length);
+                        try {
+                            socket.receive(p);
+
+                            String s = new String(p.getData(), 0, p.getLength());
+                            if (s.split("[\\r\\n]+")[0].toUpperCase().startsWith("HTTP/1.1 200")) {
+                                addresses.add(p.getAddress().getHostAddress());
+                            }
+                        }
+                        catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        curTime = System.currentTimeMillis();
+                    }
+                    socket.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                lock.release();
+            }
+            return addresses.toArray(new String[0]);
+        });
     }
 
-
-    @Override
-    protected Object doInBackground(Object[] params) {
-
-        WifiManager wifi = (WifiManager)ctx.getSystemService( ctx.getApplicationContext().WIFI_SERVICE );
-
-        if(wifi != null) {
-
-            WifiManager.MulticastLock lock = wifi.createMulticastLock("The Lock");
-            lock.acquire();
-
-            DatagramSocket socket = null;
-
-            try {
-
-                InetAddress group = InetAddress.getByName("239.255.255.250");
-                int port = 1900;
-                String query =
-                        "M-SEARCH * HTTP/1.1\r\n" +
-                                "HOST: 239.255.255.250:1900\r\n"+
-                                "MAN: \"ssdp:discover\"\r\n"+
-                                "MX: 1\r\n"+
-                                "ST: urn:schemas-upnp-org:service:AVTransport:1\r\n"+  // Use for Sonos
-                                //"ST: ssdp:all\r\n"+  // Use this for all UPnP Devices
-                                "\r\n";
-
-                socket = new DatagramSocket(port);
-                socket.setReuseAddress(true);
-
-                DatagramPacket dgram = new DatagramPacket(query.getBytes(), query.length(),
-                        group, port);
-                socket.send(dgram);
-
-                long time = System.currentTimeMillis();
-                long curTime = System.currentTimeMillis();
-
-                // Let's consider all the responses we can get in 1 second
-                while (curTime - time < 1000) {
-                    DatagramPacket p = new DatagramPacket(new byte[12], 12);
-                    socket.receive(p);
-
-                    String s = new String(p.getData(), 0, p.getLength());
-                    if (s.toUpperCase().equals("HTTP/1.1 200")) {
-                        addresses.add(p.getAddress().getHostAddress());
-                    }
-
-                    curTime = System.currentTimeMillis();
-                }
-
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                socket.close();
-            }
-            lock.release();
+    public static String[] discoverDevices(Context ctx, String serviceType ) {
+        try {
+            return discoverDevicesAsync(ctx, serviceType).get(2000, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            return null;
         }
-        return null;
+    }
+
+    public static String[] discoverAllDevices(Context ctx) {
+        return discoverDevices(ctx, ST_ALL);
+    }
+
+    public static String[] discoverRootDevices(Context ctx) {
+        return discoverDevices(ctx, ST_ROOTDEVICE);
     }
 
     public static String[] discoverDevices(Context ctx) {
-        UPnPDiscovery discover = new UPnPDiscovery(ctx);
-        discover.execute();
-        try {
-            Thread.sleep(1500);
-            return discover.addresses.toArray(new String[discover.addresses.size()]);
-        } catch (InterruptedException e) {
-            return null;
-        }
+        // Compatibility API for original Sonos application
+        return discoverDevices(ctx, ST_AVTRANSPORT);
     }
 }
